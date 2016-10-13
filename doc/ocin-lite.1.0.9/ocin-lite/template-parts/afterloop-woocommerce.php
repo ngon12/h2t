7058a33b69cc73caf43cdd8c208ec578cad3bcb4
ngon12 <ducloc.mms25@gmail.com>
2016-10-13 13:02:44 +0700

    git push

--- doc/ocin-lite.1.0.9/ocin-lite/template-parts/afterloop-woocommerce.php
+++
@@ -1,8 +0,0 @@
-					<div class="clearfix"></div>
-					<?php
-		            if ( ! is_shop() && ! is_product() && ! is_product_category() && ! is_product_tag() ) {
-		                echo '</div><!-- /ql_background -->';
-		            }
-		            ?>
-				
-        	</div><!-- /content -->
